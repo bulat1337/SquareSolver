161704bfc414bd91c67045f914f0a38fb27600a3
bulat1337 <bulatmot@gmail.com>
2023-08-17 23:28:34 +0300

    Переписал код согласно указаниям

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-void read_coefs(double * a, double * b, double * c);
+
+void read_coefs(double * a,double * b,double * c);
+void quadratic(double a, double b, double discriminant, double * x1, double * x2, int * flag);
+void linear(double b, double c, double * x1);
+void solve(double a, double b, double c, double * x1, double * x2, int * flag);
+void answer(int flag, double x1, double x2);
 double solve_discriminant(double a, double b, double c);
-void linear(double * b, double * c);
-void quadratic(double * a, double * b, double * discriminant);
 
 int main()
 {
-    double a = 0.0, b = 0.0, c = 0.0, discriminant = 0.0;
-    char answer = 'n';
-
+    double a = 0.0, b = 0.0, c = 0.0,x1 = 0.0, x2 = 0.0, discriminant = 0.0;
+    int flag = 0;
+    char ans = 'n';
     printf("Hi! U wanna solve a quadratic equation?(enter \"y\" if u do):");
-
-    while((answer = getchar()) == 'y')
+    while((ans = getchar()) == 'y')
     {
-        read_coefs(&a, &b, &c);
-
-        if(a == 0)
-        {
-            linear(&b, &c);
-        }
-        else
-        {
-            discriminant = solve_discriminant(a, b, c);
-            quadratic(&a, &b, &discriminant);
-        }
-
-        while(getchar() != '\n');
+        read_coefs(&a,&b,&c);
+        solve(a, b, c, &x1, &x2, &flag);
+        answer(flag, x1, x2);
+        flag = 0;
 
         printf("U wanna solve another?\n");
+        while(getchar() != '\n');
     }
     printf("Thats it! Bye!");
 
@@ -46,44 +40,76 @@ void read_coefs(double * a,double * b,double * c)
         scanf("%lf", c);
 }
 
-double solve_discriminant(double a, double b,double c)
+void solve(double a, double b, double c, double * x1, double * x2, int * flag)
+{
+    double discriminant;
+    if(a == 0)
+    {
+        if(b == 0)
+        {
+            *flag = 3;
+        }
+        else
+        {
+            *flag = 1;
+            linear(b, c, x1);
+        }
+    }
+    else
+    {
+        discriminant = solve_discriminant(a, b, c);
+        quadratic(a, b, discriminant, x1, x2, flag);
+    }
+}
+
+void linear(double b, double c, double * x1)
+{
+    *(x1) = -(c / b);
+}
+
+void quadratic(double a, double b, double discriminant, double * x1, double * x2, int * flag)
+{
+    if(discriminant > 0)
+            {
+                *x1 = (-b + sqrt(discriminant)) / (2 * a);
+                *x2 = (-b - sqrt(discriminant)) / (2 * a);
+            }
+            else if(discriminant == 0)
+            {
+                *flag = 1;
+                *x1 = (-b + sqrt(discriminant)) / (2 * a);
+            }
+            else
+            {
+                *flag = 2;
+            }
+}
+
+void answer(int flag, double x1, double x2)
+{
+    if(flag == 0)
+    {
+        printf("x1 = %.6lf\n",x1);
+        printf("x2 = %.6lf\n",x2);
+    }
+    else if(flag == 1)
+    {
+        printf("x = %.6lf\n",x1);
+    }
+    else if(flag == 2)
+    {
+        printf("No root.\n");
+    }
+    else
+    {
+        printf("The equation with such coefficients is impossible.\n");
+    }
+}
+
+double solve_discriminant(double a, double b, double c)
 {
     double discriminant = 0.0;
     discriminant = (b * b) - (4 * a * c);
 
     return discriminant;
-}
-
-void linear(double * b, double * c)
-{
-    double x = 0.0;
-    if(*b == 0)
-    {
-        printf("The equation with such coefs is impossible!\n");
-    }
-    else
-    {
-        x = -(*c / *b);
-        printf("x = %.6lf\n", x);
-    }
-}
-
-void quadratic(double * a, double * b, double * discriminant)
-{
-    double x1 = 0.0, x2 = 0.0;
-    if((*discriminant) > 0)
-            {
-                x1 = (-(*b) + sqrt((*discriminant))) / (2 * (*a));
-                x2 = (-(*b) - sqrt((*discriminant))) / (2 * (*a));
-                printf("x1 = %.6lf\nx2 = %.6lf\n", x1, x2);
-            }
-            else if((*discriminant) == 0)
-            {
-                x1 = (-(*b) + sqrt((*discriminant))) / (2 * (*a));
-                printf("x = %.6lf\n", x1);
-            }
-            else
-            {
-                printf("no root\n");
-            }
 }
